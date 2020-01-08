5f3643e1c21a07644c1f840a0064f5968ccc9b16
Peng <pengxu.he@happyelements.com>
2020-01-08 18:38:45 +0800

    hma

--- src/research/common/backtest.py
+++ src/research/common/backtest.py
@@ -1,3 +1,6 @@
+import math
+
+
 class BackTest():
     STATE_EMPTY = 0
     STATE_HOLD = 1
@@ -5,8 +8,12 @@ class BackTest():
     def __init__(self):
         self.state = self.STATE_EMPTY
         self.discount = 0.999  # remain 998 when earn 1000$
-        self.money = 10000
+        self.init_money = 10000
+        self.money = self.init_money
         self.hold = 0
+        self.days = 0
+        self.trading_up_times = 0
+        self.trading_times = 0
 
     def test(self, signal_table):
         """
@@ -14,7 +21,9 @@ class BackTest():
         :param signal_table: signal table is a pandas DataFrame with 4 columns. "trade_date" + "close" + "buy" + "sell"
         :return:
         """
+        self.days = len(signal_table)
         last_price = 0.
+        buy_price = 0.
         for index, row in signal_table.iterrows():
             buy_signal = True if row['buy'] == 1 else False
             sell_signal = True if row['sell'] == 1 else False
@@ -22,6 +31,7 @@ class BackTest():
             close = row['close']
             last_price = close
             if self.state == self.STATE_EMPTY and buy_signal:
+                buy_price = close
                 shares = self.money / close
                 self.money = 0
                 self.hold = shares
@@ -29,11 +39,19 @@ class BackTest():
                 print("[date={}] buy {} shares at price = {}".format(date, shares, close))
 
             elif self.state == self.STATE_HOLD and sell_signal:
+                if close > buy_price:
+                    self.trading_up_times = self.trading_up_times + 1
                 self.money = int((self.money + self.hold * close) * self.discount)
                 self.state = self.STATE_EMPTY
 
                 print("[date={}] sell {} shares at price = {}".format(date, self.hold, close))
                 self.hold = 0
+                self.trading_times = self.trading_times + 1
 
         value = self.money if self.money > 0 else self.hold * last_price
+        print(" ===== backtest done =====")
+        print("total days = {} ".format(self.days))
+        # print("trade times = {}".format(self.trading_times))
         print("final money = {}".format(value))
+        print("{}/{}. succ rate = {} ".format(self.trading_up_times, self.trading_times , (self.trading_up_times * 1.) / (self.trading_times * 1.)))
+        print("annual ror = {}".format(pow((value * 1.) / (self.init_money * 1.), 1. / (self.days * 1. / 250.)) - 1.))
--- src/research/timing.py
+++ src/research/timing.py
@@ -7,6 +7,7 @@ from src.context import context
 from src.utils.share_selector import ShareSelector
 from common.backtest import BackTest
 
+
 def hma(y, N):
     return ta.WMA((2 * ta.WMA(y, N / 2) - ta.WMA(y, N)), int(math.sqrt(N)))
 
@@ -14,6 +15,8 @@ def hma(y, N):
 index = "000001.SH"
 
 target_column = 'score'
+k_source_column = 'k_source_column'
+
 df = context.tushare.index_daily(ts_code='000001.SH', start_date='20160101', end_date='20161231')
 df = df.iloc[::-1]
 df = df.loc[:, ['trade_date', 'close', 'vol']]
@@ -24,34 +27,31 @@ df['sma20'] = ta.SMA(df[target_column], timeperiod=20)
 
 df['hma5'] = hma(df['close'], 5)
 df['hma10'] = hma(df['close'], 10)
-df['hma20'] = hma(df['close'], 60)
 
-# buy_signal = df["sma5"] > df["sma10"]
-# df['up'] = df["sma5"] > df["sma10"]
-# df.where(buy_signal, inplace = True)
-day_offset = 1
-short_metric = 'sma5'
-long_metric = 'sma10'
+df[k_source_column] = hma(df['score'], 35)
+df.index = df['trade_date']
+
+# day_offset = 1
+# short_metric = 'sma5'
+# long_metric = 'sma10'
 # df['buy'] = np.where((df[short_metric].shift(day_offset) < df[long_metric].shift(day_offset)) &
 #                      (df[short_metric] > df[long_metric]), 1, 0)
 # df['sell'] = np.where((df[short_metric].shift(day_offset) > df[long_metric].shift(day_offset)) &
 #                       (df[short_metric] < df[long_metric]), 1, 0)
-df.index = df['trade_date']
 
 # print df
 # df.loc[:, ['trade_date', 'buy', 'sell']].to_csv('/tmp/r.csv')
 # print ta.SMA(df["close"], timeperiod=25)
 
-k_metric = 'hma20'
-df.loc[:, ['close', k_metric]].plot(secondary_y=True, grid=True, figsize=(10, 6))
+df.loc[:, ['close', k_source_column]].plot(secondary_y=True, grid=True, figsize=(10, 6))
 plt.savefig('books_read.png')
 plt.show()
 
-df['k'] = df[k_metric] - df[k_metric].shift(1)
+df['k'] = df[k_source_column] - df[k_source_column].shift(1)
 df['buy'] = np.where(df['k'] > 0, 1, 0)
 df['sell'] = np.where(df['k'] < 0, 1, 0)
 df['close'] = df['close'] / 1000
-df = df.loc[:, ['trade_date', 'close', 'hma5', 'k', 'buy', 'sell']]
+df = df.loc[:, ['trade_date', 'close', 'k', 'buy', 'sell']]
 print df
 
 bt = BackTest()
